a77117d4330da4345134e8e492c098129a14ffa4
Niraj Sangroula <sangroulaniraj15@gmail.com>
2025-08-08 03:06:06 -0400

    Setup debezium - for mydb.offer -> created docker-compose, to also setup initialization.sql, for generating debezium user - And configure variables in mysql - via my.in file. Now, at least for now, event is correctly published by Debezium using kafka.

---
+++ bikar-offer-deal/src/main/java/com/pm/niraj/bikarorderdeal/DebeziumConfig.java
@@ -0,0 +1,88 @@
+package com.pm.niraj.bikarorderdeal;
+
+import io.debezium.engine.ChangeEvent;
+import io.debezium.engine.DebeziumEngine;
+import io.debezium.engine.format.Json;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@Configuration
+public class DebeziumConfig {
+
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private DebeziumEngine<ChangeEvent<String, String>> engine;
+    private Future<?> engineTask;
+
+    @Autowired(required = false)
+    private org.springframework.kafka.core.KafkaTemplate<String, String> kafkaTemplate;
+
+
+    @Bean
+    public DebeziumEngine<ChangeEvent<String, String>> debeziumEngine() {
+
+        Properties props = new Properties();
+        props.setProperty("name", "embedded-engine");
+        props.setProperty("connector.class", "io.debezium.connector.mysql.MySqlConnector");
+        props.setProperty("database.hostname", "localhost");
+        props.setProperty("database.port", "3306");
+        props.setProperty("database.user", "debezium");
+        props.setProperty("database.password", "dbzpassword");
+        props.setProperty("database.server.id", "85744");
+        props.setProperty("database.server.name", "myserver");
+        props.setProperty("database.include.list", "mydb");
+        props.setProperty("table.include.list", "mydb.offer");
+
+        // Use Kafka for schema history storage
+        props.setProperty("schema.history.internal", "io.debezium.storage.kafka.history.KafkaSchemaHistory");
+        props.setProperty("schema.history.internal.kafka.bootstrap.servers", "localhost:9092");
+        props.setProperty("schema.history.internal.kafka.topic", "schema-changes.myapp");
+
+        // Topic prefix for CDC events
+        props.setProperty("topic.prefix", "myapp");
+
+        // Offset storage to track binlog progress
+        props.setProperty("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore");
+        props.setProperty("offset.storage.file.filename", "/tmp/debezium-offsets.dat");
+
+        // Recommended converters
+        props.setProperty("key.converter", "org.apache.kafka.connect.json.JsonConverter");
+        props.setProperty("value.converter", "org.apache.kafka.connect.json.JsonConverter");
+        props.setProperty("key.converter.schemas.enable", "false");
+        props.setProperty("value.converter.schemas.enable", "false");
+
+        engine = DebeziumEngine.create(Json.class)
+                .using(props)
+                .notifying(this::handleChangeEvent)
+                .build();
+
+        engineTask = executor.submit(engine);
+        return engine;
+    }
+
+    private void handleChangeEvent(ChangeEvent<String, String> changeEvent) {
+        String eventValue = changeEvent.value();
+
+        System.out.println("CDC event received: " + eventValue);
+
+        // Optional: send event to Kafka topic "cdc-topic"
+        if (kafkaTemplate != null) {
+            kafkaTemplate.send("cdc-topic", eventValue);
+        }
+    }
+
+    @PreDestroy
+    public void shutdown() throws IOException {
+        if (engine != null) {
+            engine.close();
+        }
+        executor.shutdownNow();
+    }
+}
