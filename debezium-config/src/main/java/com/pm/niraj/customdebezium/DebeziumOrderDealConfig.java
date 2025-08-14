c0dc0b0f0374327066f56236a1d38841152476a0
Niraj Sangroula <sangroulaniraj15@gmail.com>
2025-08-14 19:27:42 -0400

    Configured kafka-topic with cdc-topic.name for debezium cdc-topic

--- debezium-config/src/main/java/com/pm/niraj/customdebezium/DebeziumOrderDealConfig.java
+++ debezium-config/src/main/java/com/pm/niraj/customdebezium/DebeziumOrderDealConfig.java
@@ -1,6 +1,7 @@
 package com.pm.niraj.customdebezium;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.Profile;
 @Profile("!test")
 @ConditionalOnProperty(name = "debezium.enabled", havingValue = "true", matchIfMissing = false)
 public class DebeziumOrderDealConfig extends DebeziumConfig {
+    @Value("${cdc-topic.name}")
+    private String kafkaTopic;
     @Autowired(required = false)
     private org.springframework.kafka.core.KafkaTemplate<String, String> kafkaTemplate;
 
@@ -17,10 +20,10 @@ public class DebeziumOrderDealConfig extends DebeziumConfig {
     protected CustomDebeziumParams getCustomDebeziumParams() {
         return (key, value) -> {
             String eventValue = value;
-            System.out.println("CDC event received: " + eventValue);
+            System.out.println("CDC event received: " + eventValue + " in topic " + kafkaTopic);
             // Optional: send event to Kafka topic "cdc-topic"
             if (kafkaTemplate != null) {
-                kafkaTemplate.send("cdc-topic", eventValue);
+                kafkaTemplate.send(kafkaTopic, eventValue);
             }
         };
     }
